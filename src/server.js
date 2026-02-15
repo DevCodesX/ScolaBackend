c64164372725b2c256c0feca3d4a9c6342efc46a
Mohamed <m07am0mar@gmail.com>
2026-02-15 16:59:43 -0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -18,7 +18,10 @@ const db = require('./config/db.js');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5173", "http://localhost:3000"],
+    credentials: true,
+}));
 app.use(express.json());
 
 // Health check route
